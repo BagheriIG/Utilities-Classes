0e493a8788cadb07b3c8c6450e5c38b147bc57e8
Mohicode <90272855+BagheriIG@users.noreply.github.com>
2025-02-15 10:25:22 +0330

    Add ListHelper class
    Currently contains GroupByFunction to create List of Lists by comparing function

---
+++ Utilities/ListHelper.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilities
+{
+    // Usage Example:
+    // var groupedByProperty = ListHelper.GroupByFunction(MyList, (item1, item2) => item1.MyProperty == item2.MyProperty);
+    public static class ListHelper
+    {
+        public static List<List<T>> GroupByFunction<T>(List<T> items, Func<T, T, bool> condition)
+        {
+            var groupedResults = new List<List<T>>();
+            foreach (var item in items)
+            {
+              bool found = false;
+              foreach (var group in groupedResults)
+              {
+                if (condition(group[0], item))
+                {
+                  group.Add(item);
+                  found = true;
+                  break;
+                }
+              }
+              if (!found)
+              {
+                groupedResults.Add(new List<T> {item});
+              }
+            }
+            return groupedResults;
+        }
+    }
+}
